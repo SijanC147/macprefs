6b6973a051d769220b95e5da279c29eb8009e1c5
Sean Bugeja <seanbugeja23@gmail.com>
2023-08-01 16:30:14 +0200

    fix script

--- bump.sh
+++ bump.sh
@@ -27,12 +27,12 @@ function bump_version() {
   new_version="${version_parts[0]}.${version_parts[1]}.${version_parts[2]}"
 
   # Update the version in the file
-  sed -i "" "s/${current_version}/${new_version}/" macprefs/__init__.py
-  .venv/bin/black macprefs/__init__.py
+  sed -i "" "s/${current_version}/${new_version}/" macprefs/__init__.py &>/dev/null
+  .venv/bin/black macprefs/__init__.py &>/dev/null
 
-  git add macprefs/__init__.py
-  git commit -m "${bump_type^^} Bump version to ${new_version}"
-  git push origin master
+  git add macprefs/__init__.py &>/dev/null
+  git commit -m "${bump_type^^} Bump version to ${new_version}" &>/dev/null
+  git push origin master &>/dev/null
 
   echo -n "$new_version"
 }
