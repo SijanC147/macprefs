4732e84a47172a603025f4f24352f96f8f606758
Sean Bugeja <seanbugeja23@gmail.com>
2023-08-01 13:38:35 +0200

    update template

--- assets/macprefs.template.rb
+++ assets/macprefs.template.rb
@@ -6,10 +6,15 @@ class Macprefs < Formula
   url "https://github.com/sijanc147/macprefs/archive/###version###.tar.gz"
   sha256 "###sha256###"
 
-  depends_on "python3"
+  depends_on "python"
+
+  resource "mock" do
+    url "https://files.pythonhosted.org/packages/66/ab/41d09a46985ead5839d8be987acda54b5bb93f713b3969cc0be4f81c455b/mock-5.1.0.tar.gz"
+    sha256 "5e96aad5ccda4718e0a229ed94b2024df75cc2d55575ba5762d31f5767b8767d"
+  end
 
   def install
-    bin.install Dir["*"]
+    virtualenv_install_with_resources
   end
 
   test do
