8eabc6d88f189739f3152bb775466aa56093b3b2
GitHub Action <action@github.com>
2023-08-01 14:55:51 +0000

    Auto update version and sha256 for release v2.0.20

--- Formula/macprefs.rb
+++ Formula/macprefs.rb
@@ -3,8 +3,8 @@ class Macprefs < Formula
 
   desc "Backup and Restore your Mac System and App Preferences"
   homepage "https://github.com/sijanc147/macprefs"
-  url "https://github.com/SijanC147/macprefs/releases/download/v2.0.18/macprefs-2.0.18.tar.gz"
-  sha256 "63c3bf4dd6a72e431555e2930567e42176c27fc45b6c0e7551220604b4add74a"
+  url "https://github.com/SijanC147/macprefs/releases/download/v2.0.20/macprefs-2.0.20.tar.gz"
+  sha256 "9f65ea6dfd1ed25b4a9c875670f4c1ae585972108531a3027b319ed55c64757f"
 
   depends_on "python"
 
